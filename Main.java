b514ce736c6770025b75d4aa7adce2e79c275919
b4s14rd <fearlessforever@mail.ru>
2025-11-18 10:24:13 +0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String dataFilePath = "instruction_patching_data.txt";
+
+        QuestionRepository repository = new QuestionRepository(dataFilePath);
+        DialogLogic logic = new DialogLogic(repository);
+        ConsoleInterface console = new ConsoleInterface(logic);
+
+        console.startInteraction(); //взаимодействие
+    }
+}
