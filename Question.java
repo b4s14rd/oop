766e15fbe383a9b0838387efc0b04b808836a78d
b4s14rd <fearlessforever@mail.ru>
2025-12-01 20:09:45 +0500

    Create Question.java

---
+++ Question.java
@@ -0,0 +1,12 @@
+public class Question {
+    private String title;
+    private String answer;
+
+    public Question(String title, String answer) {
+        this.title = title;
+        this.answer = answer;
+    }
+
+    public String getTitle() { return title; }
+    public String getAnswer() { return answer; }
+}
